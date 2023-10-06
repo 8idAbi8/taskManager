ff0117b42f6e208bc217959bcd4e407646471390
8idAbi8 <i.l.t.l.i@hotmail.it>
2023-10-06 12:06:02 +0200

    bootstrap

---
+++ src/main/java/com/example/fullstack/model/Project.java
@@ -0,0 +1,8 @@
+package com.example.fullstack.model;
+
+/**
+ * Created by idlirabilaliaj on 06/10/23.
+ * Github: https://github.com/8idAbi8
+ */
+public class Project {
+}
---
+++ src/main/java/com/example/fullstack/user/User.java
@@ -0,0 +1,8 @@
+package com.example.fullstack.user;
+
+/**
+ * Created by idlirabilaliaj on 06/10/23.
+ * Github: https://github.com/8idAbi8
+ */
+public class User {
+}
