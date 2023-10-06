415c3df4200df81f73a6d322461793c0a9e8007b
8idAbi8 <i.l.t.l.i@hotmail.it>
2023-10-06 12:23:57 +0200

    working on  User

--- src/main/java/com/example/fullstack/user/User.java
+++ src/main/java/com/example/fullstack/user/User.java
@@ -1,8 +1,36 @@
 package com.example.fullstack.user;
 
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
 /**
  * Created by idlirabilaliaj on 06/10/23.
  * Github: https://github.com/8idAbi8
  */
-public class User {
+@Entity
+@Table(name = "users")
+public class User extends PanacheEntity {
+
+    @Column(unique = true, nullable = false)
+    public String name;
+
+    @Column(nullable = false)
+    String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    public ZonedDateTime created;
+
+    @Version
+    public int version;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "role")
+    public List<String> roles;
+
 }
